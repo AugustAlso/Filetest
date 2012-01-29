8c70b1c6624cc50b5fbec278ab036a0dcd7e3c34
Austin Bookheimer <Austin.bookheimer@gmail.com>
2012-01-28 19:50:32 -0700

    Import nobel winners and print them out, yo.

--- src/main/java/com/augustalso/Main.java
+++ src/main/java/com/augustalso/Main.java
@@ -1,13 +1,28 @@
 package com.augustalso;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		CSVReader reader = new CSVReader(new FileReader("nobelWinners.csv"));
+	    List<String[]> myEntries = reader.readAll();
+	    
+	    for (String[] columns : myEntries){
+	    	for (String column : columns){
+	    		System.out.println("Column: " + column);
+	    	}
+	    }
 	}
 
 }
