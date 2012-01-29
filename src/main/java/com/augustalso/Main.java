0199928098eb440e5eaa506dc9f97865354b3fa4
Austin Bookheimer <Austin.bookheimer@gmail.com>
2012-01-28 20:23:04 -0700

    Added NobelWinner class to slurp up our nobel winner CSV data

--- src/main/java/com/augustalso/Main.java
+++ src/main/java/com/augustalso/Main.java
@@ -3,6 +3,7 @@ package com.augustalso;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import au.com.bytecode.opencsv.CSVReader;
@@ -14,15 +15,22 @@ public class Main {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+		List<NobelWinner> winners = new ArrayList<NobelWinner>();
+		
 		CSVReader reader = new CSVReader(new FileReader("nobelWinners.csv"));
 	    List<String[]> myEntries = reader.readAll();
 	    
 	    for (String[] columns : myEntries){
-	    	for (String column : columns){
-	    		System.out.println("Column: " + column);
-	    	}
+	    	NobelWinner winner = new NobelWinner();
+	    	winner.setName(columns[0]);
+	    	winner.setYear(columns[1]);
+	    	winners.add(winner);
 	    }
+	    
+	    for (NobelWinner winner : winners){
+	    	System.out.println(winner.getName());
+	    }
+	    
 	}
 
 }
---
+++ src/main/java/com/augustalso/NobelWinner.java
@@ -0,0 +1,22 @@
+package com.augustalso;
+
+public class NobelWinner {
+	private String name;
+	private String year;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
+	
+
+}
